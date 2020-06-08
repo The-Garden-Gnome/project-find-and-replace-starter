313bc73461be047ffe40933296c527d317a53b04
Samantha Taylor <staylor@mort.is>
2020-06-08 14:17:44 -0400

    Created loops to loop over everything. Last loop has an if statement that does the replacing.

--- main.js
+++ main.js
@@ -1,26 +1,34 @@
-// You should NOT change the HTML or CSS in this project (at least until you reach
-// the bonus objectives). Focus on the JavaScript.
-
 const findInput = document.querySelector(".find-input")
 const replaceInput = document.querySelector(".replace-input")
 const replaceAllButton = document.querySelector(".replace-all-button")
-
-// The following variable holds your OUTER ARRAY of row elements.
-// Later you will need an OUTER LOOP to loop over the individual elements within
-// this array.
 const rowElements = document.querySelectorAll(".row")
 
-// When you call the function belwo, it will get and return an INNER ARRAY
-// containing the cell elements for a given row.
-// Call this function from WITHIN your row elements loop. Then you will, in turn,
-// need to loop over the resulting cell elements. But where should this whole
-// NESTED LOOP go? Think through the user's experience: when should WHAT happen? 
 function getCellElements (currentRowElement) {
     return currentRowElement.querySelectorAll(".cell")
 }
 
+replaceAllButton.addEventListener("click", function (){
+    const find = findInput.value;
+    const replace = replaceInput.value;
+    for (let rowindex = 0; rowindex < rowElements.length; rowindex++) {
+        let nameList = getCellElements(rowElements[rowindex])
+        for (let cellIndex = 0; cellIndex < nameList.length; cellIndex++){
+            if (nameList[cellIndex].innerText.includes(find)) {
+                nameList[cellIndex].innerHTML = nameList[cellIndex].innerHTML.replace(find, replace)
+            }
+        }
+}
+})
 
-// YOUR CODE GOES HERE
+// Requirements
+// Add a click event listener to the replaceAllButton.
+// On click but outside of a loop, assign the values of the two input textboxes ( findInput and replaceInput), so that you are only retrieving these values once per click.
+// Write a loop which loops over the rowElements array (which is already provided in the starter code).
+// Inside this loop, use the getCellElements() function (already provided in the starter code) and assign the resulting array of cell elements to a variable.
+// Write a nested loop which loops over the array of cell elements.
+// For each cell element, check if a cell contains the user-provided search string. Use the string method includes().
+// If a cell does contain the user-provided search string, use innerHTML and the string method replace() to replace the user-provided search string with the user-provided replacement string.
+// In all your loops, avoid using low-meaning index variables like "i" and "j".
 
 
 // One last thing: dedicate very careful attention to using variables and
